482aefd76aba236108188954ad386236d2cb5a1f
Akhilgovind02 <akhilgovind846@gmail.com>
2023-11-17 15:20:43 +0530

    respHomepage

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -8,6 +8,16 @@ import { Image } from 'react-bootstrap';
 import logo from './dthLogo.png'
 import { Link } from 'react-router-dom';
 // import './Home.css'
+import { NavLink } from 'react-router-dom';
+
+import {
+  CDBSidebar,
+  CDBSidebarContent,
+  CDBSidebarFooter,
+  CDBSidebarHeader,
+  CDBSidebarMenu,
+  CDBSidebarMenuItem,
+} from 'cdbreact';
 import styles from './home.module.css'
 import BusinessIcon from '@mui/icons-material/Business';
 import RecyclingIcon from '@mui/icons-material/Recycling';
@@ -15,44 +25,53 @@ import SummarizeIcon from '@mui/icons-material/Summarize';
 import LogoutIcon from '@mui/icons-material/Logout';
 function Home() {
   return (
-    <div>
-    <Navbar expand="lg" id={styles.nav} className="bg-body-tertiary">
-      <Container fluid>
-        <Link to="/home"><Navbar.Brand ><Image style={{width: '90px'}} src={logo}></Image></Navbar.Brand></Link>
-        <Navbar.Toggle aria-controls="navbarScroll" />
-        <Navbar.Collapse id="navbarScroll">
-          <Nav
-            className="me-auto my-2 my-lg-0"
-            style={{ maxHeight: '100px' }}
-            navbarScroll
-          >
-            <Nav.Link href="#action1">Home</Nav.Link>
-            <Nav.Link href="#action2">Link</Nav.Link>
-            <NavDropdown title="Link" id="navbarScrollingDropdown">
-              <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action4">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action5">
-                Something else here
-              </NavDropdown.Item>
-            </NavDropdown>
-            <Nav.Link href="#" disabled>
-              Link
-            </Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-    <div className={styles.main}>
-
-      <Button style={{backgroundColor:'rgb(120, 59, 234)'}} className={styles.btnm}><BusinessIcon style={{width:'40px', height:'40px', margin:'10px'}}></BusinessIcon>Standard</Button>
-      <Button style={{backgroundColor:'rgb(29, 157, 80)'}} className={styles.btnm}><RecyclingIcon style={{width:'40px' ,height:'40px', margin:'10px'}}></RecyclingIcon>Recycle</Button>
-      <Button style={{backgroundColor:'rgb(140, 65, 164)'}} className={styles.btnm}><SummarizeIcon style={{width:'40px', height:'40px' ,margin:'10px'}}></SummarizeIcon>Report</Button>
-      <Button style={{backgroundColor:'rgb(250, 112, 62)'}} className={styles.btnm}><LogoutIcon style={{width:'40px' ,height:'40px', margin:'10px'}}></LogoutIcon>Logout</Button>
+    <Container fluid className={styles.main}>
+    
+    <div style={{ display: 'flex', height: '87vh', overflow: 'scroll initial'}}>
+      <div>
+        <CDBSidebar textColor="black" backgroundColor="rgba(240, 240, 240, 0.5)" >
+          <CDBSidebarHeader prefix={<i className="fa fa-bars fa-large"></i>}>
+          <Image style={{width: '90px'}} src={logo}></Image>
+          </CDBSidebarHeader>
+          <CDBSidebarContent className="sidebar-content">
+            <CDBSidebarMenu>
+              <NavLink exact to="/" activeClassName="activeClicked">
+                <CDBSidebarMenuItem icon="columns">Dashborad</CDBSidebarMenuItem>
+              </NavLink>
+              <NavLink exact to="/tables" activeClassName="activeClicked">
+                <CDBSidebarMenuItem icon="table">Standard</CDBSidebarMenuItem>
+              </NavLink>
+              <NavLink exact to="/profile" activeClassName="activeClicked">
+                <CDBSidebarMenuItem icon="user">Recycle</CDBSidebarMenuItem>
+              </NavLink>
+              <NavLink exact to="/analytics" activeClassName="activeClicked">
+                <CDBSidebarMenuItem icon="chart-line">Reports</CDBSidebarMenuItem>
+              </NavLink>
+              <NavLink exact to="/analytics" activeClassName="activeClicked">
+                <CDBSidebarMenuItem icon="arrow-right">Log out</CDBSidebarMenuItem>
+              </NavLink>
+              <NavLink exact to="/hero404" target="_blank" activeClassName="activeClicked">
+                <CDBSidebarMenuItem icon="exclamation-circle">404 page</CDBSidebarMenuItem>
+              </NavLink>
+            </CDBSidebarMenu>
+          </CDBSidebarContent>
+          <CDBSidebarFooter style={{ textAlign: 'center' }}>
+            <div
+              style={{
+                padding: '20px 5px',
+              }}
+            >
+            </div>
+          </CDBSidebarFooter>
+        </CDBSidebar>
+      </div>
+      </div>
+      <div className={styles.mainr}>
+      <div className={styles.cntn}></div>
+    <footer style={{height:"61px"}} className='bg-dark navbar-dark'></footer>
     </div>
-    </div>
+    </Container>
+  
   );
 };
 
